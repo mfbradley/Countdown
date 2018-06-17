45567e5472b1ab2d947839ff8076c68cbe362bdd
mfbradley <mbradleystylist@gmail.com>
2018-06-17 15:38:39 -0400

    updated js

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -1,25 +1,33 @@
-function updateTime() {
-    var a = moment(new Date());
-    var b = moment('2018, 06, 21').toObject();
-    var duration = moment.duration(-a.diff(b))
+$(document).ready(function() {
 
-    console.log(duration)
- 
-    var days = duration._data.days;
-    var hours = duration._data.hours;
-    var minutes = duration._data.minutes;
-    var seconds = duration._data.seconds;
-    var milliseconds = duration._data.milliseconds;
+    function updateTime() {
+        var a = moment(new Date());
+        var b = moment('2018, 06, 21').toObject();
+        var duration = moment.duration(-a.diff(b))
 
-    var msConverted = moment(milliseconds).format('SS')
-    var html = days + ' d ' + hours + ' h ' + minutes + ' m ' + seconds + ' s ' + msConverted + ' ms'
+        console.log(duration)
 
-    $("#time").text(html);
-    // $("#milliseconds").text(ms);
-}
+        var days = duration._data.days;
+        var hours = duration._data.hours;
+        var minutes = duration._data.minutes;
+        var seconds = duration._data.seconds;
+        var milliseconds = duration._data.milliseconds;
 
-setInterval(function () {
+        var msConverted = moment(milliseconds).format('SS')
+        var html = days + ' d ' + hours + ' h ' + minutes + ' m ' + seconds + ' s ' + msConverted + ' ms'
+
+        $("#time").text(html);
+        // $("#milliseconds").text(ms);
+    }
+
+    setInterval(function () {
+        updateTime();
+    }, 10);
+
     updateTime();
-}, 10);
 
-updateTime();
+    $(".btn").click(function () {
+        alert("clicked!");
+    })
+
+});
